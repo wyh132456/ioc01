dde5f8bfcd2ed376ac59d1c5c9c92f0716bddde3
wyh123456 <381985997@qq.com>
2021-10-09 17:46:10 +0800

    spring learn

--- src/main/java/org/javaboy/ioc/javaconfig/JavaConfig.java
+++ src/main/java/org/javaboy/ioc/javaconfig/JavaConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Repository;
 
 import javax.xml.ws.Service;
 
@@ -13,11 +14,10 @@ import javax.xml.ws.Service;
     java配置类，作用类似于applicationContext.xml
  **/
 @Configuration
-@ComponentScan(basePackages = "org.javaboy.ioc.service",useDefaultFilters = false,includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Service.class)})
+@ComponentScan(basePackages = "org.javaboy.ioc",useDefaultFilters = false,includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Service.class),@ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Repository.class)})
 public class JavaConfig {
     @Bean("he")
     SayHello sayHello(){
         return new SayHello();
     }
-
 }
--- src/main/java/org/javaboy/ioc/service/UserService.java
+++ src/main/java/org/javaboy/ioc/service/UserService.java
@@ -1,5 +1,7 @@
 package org.javaboy.ioc.service;
 
+import org.javaboy.ioc.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -7,7 +9,12 @@ import java.util.List;
 
 @Service
 public class UserService {
+    @Autowired
+    UserDao userDao;
     public List<String>getAllUsers(){
+
+        String hello = userDao.hello();
+        System.out.println("hello = " + hello);
         List<String> users = new ArrayList<String>();
         for (int i = 0; i < 10; i++) {
             users.add("allboy"+ i);
